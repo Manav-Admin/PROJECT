9f2db5b08f3ad5bcf8a0c5d2cecca34414dad6a9
Manav-Admin <manav.techhelp@gmail.com>
2025-04-16 10:05:42 -0700

    new

--- python/dict.py
+++ python/dict.py
@@ -54,3 +54,19 @@ def first_non_repeating_char(s):
 string = "aabbcddex"
 result = first_non_repeating_char(string)
 print("First non-repeating character:", result)
+
+def two_sum(nums, target):
+    num_map = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+
+    return None
+numbers = [2, 7, 11, 15]
+target = 9
+result = two_sum(numbers, target)
+print("Indices of two numbers that add up to target:", result)
+
