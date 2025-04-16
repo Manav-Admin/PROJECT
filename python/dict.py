140cf40d2cc038497fdb54992b4c2ea29f13df1a
Manav-Admin <manav.techhelp@gmail.com>
2025-04-16 10:10:39 -0700

    new

--- python/dict.py
+++ python/dict.py
@@ -70,3 +70,21 @@ target = 9
 result = two_sum(numbers, target)
 print("Indices of two numbers that add up to target:", result)
 
+def is_anagram(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    count = {}
+
+    for char in s1:
+        count[char] = count.get(char, 0) + 1
+
+    for char in s2:
+        if char not in count:
+            return False
+        count[char] -= 1
+        if count[char] < 0:
+            return False
+
+    return True
+
